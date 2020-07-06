0575a8be25e9d1bc434f2b2134dd492ed5bcec91
robert <protog254@gmail.com>
2020-07-06 22:08:10 +0300

    third commit

--- prac.js
+++ prac.js
@@ -4,6 +4,10 @@ const ROCK ='ROCK'
 const PAPER ='PAPER'
 const SCISSORS ='SCISSORS'
 const DEFAULT_CHOICE = ROCK
+const draw = 'DRAW'
+const computerWins = 'COMPUTER WINS'
+const playerWins = 'PLAYER WINS'
+
 let gameIsRunning = false
 
 const getPlayerChoice=function(){
@@ -19,12 +23,49 @@ const getPlayerChoice=function(){
         return selection
 }
 
+const getComputerChoice = function(){
+    const randomValue = Math.random();
+    if(randomValue < 0.34) {
+        return ROCK
+    }
+    else if (randomValue < 0.67) {
+        return PAPER
+    }
+    else{
+        return SCISSORS
+    }
+}
+
+const getWinner = function(computer,player){
+    if(computer === player){
+        return draw;
+    }
+    else if(
+        computer === ROCK && player === PAPER ||
+        computer === SCISSORS && player === ROCK ||
+        computer === PAPER && player === SCISSORS
+        )
+        {
+            return playerWins;
+        }
+    else(
+        player === ROCK && computer === PAPER ||
+        player === SCISSORS && computer === ROCK ||
+        player === PAPER && computer === SCISSORS
+        )
+        {
+            return computerWins
+        }
+}
+
 startBtn.addEventListener('click',function(){
     if (gameIsRunning) {
         return;
     }
     gameIsRunning = true
     console.log('Game is starting...')  
-    const playerSelection=getPlayerChoice()
-    console.log(playerSelection);
+    const playerSelection = getPlayerChoice()
+    const computerSelection = getComputerChoice()
+    const winner = getWinner(playerSelection,computerSelection)
+    console.log(winner);
 })
