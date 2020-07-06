abf33d137929ebae54c0d3b3456d079b16b198de
robert <protog254@gmail.com>
2020-07-06 22:03:26 +0300

    second commit

--- prac.js
+++ prac.js
@@ -1,1 +1,30 @@
+const startBtn = document.querySelector('.btn');
 
+const ROCK ='ROCK'
+const PAPER ='PAPER'
+const SCISSORS ='SCISSORS'
+const DEFAULT_CHOICE = ROCK
+let gameIsRunning = false
+
+const getPlayerChoice=function(){
+    const selection=prompt(`${ROCK}, ${PAPER}, ${SCISSORS}`).toUpperCase();
+    if (
+        selection !== ROCK &&
+        selection !== PAPER &&
+        selection !== SCISSORS
+        ){
+        alert(`invalid choice! So we chose ${ROCK} for you!`)
+        return DEFAULT_CHOICE
+        }
+        return selection
+}
+
+startBtn.addEventListener('click',function(){
+    if (gameIsRunning) {
+        return;
+    }
+    gameIsRunning = true
+    console.log('Game is starting...')  
+    const playerSelection=getPlayerChoice()
+    console.log(playerSelection);
+})
